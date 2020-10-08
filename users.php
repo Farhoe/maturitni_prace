04ea24aa3430d7a112909f1c7c9bcbc6d83d9976
Matthew <37181565+Farhoe@users.noreply.github.com>
2020-10-08 12:44:44 +0200

    Správný výpis users
    - přidáno:
       - Password
       - City
       - Address
       - id_role

--- users.php
+++ users.php
@@ -1,6 +1,7 @@
 <?php require_once "vendor" . DIRECTORY_SEPARATOR . "autoload.php";?>
 <?php require_once "header.php";?>
-    <!-- Page Heading -->
+
+
       <h1 class="h3 mb-2 text-gray-800">Tabulka uživatelů</h1>
       <div class="card shadow mb-4">
             <div class="card-body">
@@ -9,9 +10,13 @@
             <thead>
             <tr>
                 <th>ID</th>
-                <th>Email</th>
                 <th>Křestní jméno</th>
                 <th>Příjmení</th>
+                <th>Email</th>
+                <th>Heslo</th>
+                <th>Město</th>
+                <th>Adresa</th>
+                <th>Role</th>
               </tr>
             </thead>
             <tbody>  
@@ -26,16 +31,20 @@
             <?php  foreach ($users as $user) {
                 ?> <tr>
                 <td><?php echo $user->id_user;?></td>
-                <td><?php echo $user->email;?></td>
                 <td><?php echo $user->firstname;?></td>
                 <td><?php echo $user->lastname;?></td>
+                <td><?php echo $user->email;?></td>
+                <td><?php echo $user->password;?></td>
+                <td><?php echo $user->city;?></td>
+                <td><?php echo $user->address;?></td>
+                <td><?php echo $user->id_role;?></td>
                 <?php
                       } ?>     
               </tr>            
             </tbody>
           </table>
           <form action="add_user.php">
-            <input class="btn btn-primary" type="submit" value="Přidat další do UŽIVATELŮ.">
+            <input class="btn btn-primary" type="submit" value="Přidat další do USERS.">
           </form>
       </div>
       </div>
